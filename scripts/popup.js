34505521a8fba941a07274be136345a47581654c
imdhanush <dhanushdazz@gmail.com>
2020-08-09 13:20:54 +0530

    Add Proxy on click

--- scripts/popup.js
+++ scripts/popup.js
@@ -6,10 +6,28 @@ chrome.storage.sync.get('color', function(data) {
   });
 
   changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
+
+    const config = {
+      mode: 'fixed_servers',
+      rules: {
+        proxyForHttps:{
+          scheme: 'https',
+          host: '103.28.118.50'
+        },
+        bypassList: ['github.com']
+      }
+    }
+    chrome.proxy.settings.set(
+      {value: config, scope: 'regular'},
+      function() {console.log('Proxy set')});
+
+      chrome.proxy.settings.get(
+        {'incognito': false},
+        function(config) {console.log(JSON.stringify(config));});
+    // let color = element.target.value;
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    //   chrome.tabs.executeScript(
+    //       tabs[0].id,
+    //       {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    // });
   };
